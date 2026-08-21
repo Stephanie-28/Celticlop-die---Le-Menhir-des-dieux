9dece0b5002ae2e2ec242bb5f22e3be1ac2749f2
Stephanie-28 <stephannie.hanne@laplateforme.io>
2026-08-21 02:59:19 +0200

    chore: intégrer le socle public des divinités

---
+++ app/src/Controller/PublicInfrastructureController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PublicInfrastructureController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_public_pantheon_all');
+    }
+
+    #[Route('/animaux/{id}', name: 'app_public_animal_show', requirements: ['id' => '\\d+'], methods: ['GET'])]
+    #[Route('/symboles/{id}', name: 'app_public_symbole_show', requirements: ['id' => '\\d+'], methods: ['GET'])]
+    public function pendingPublicRecord(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_public_pantheon_all');
+    }
+}
---
+++ app/src/Controller/PublicPantheonController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\DieuRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PublicPantheonController extends AbstractController
+{
+    #[Route('/pantheon', name: 'app_public_pantheon_all', methods: ['GET'])]
+    public function all(DieuRepository $repository): Response
+    {
+        return $this->renderPantheon($repository, null, 'Toutes les divinités');
+    }
+
+    #[Route('/pantheon/gaulois', name: 'app_public_pantheon_gaulois', methods: ['GET'])]
+    public function gaulish(DieuRepository $repository): Response
+    {
+        return $this->renderPantheon($repository, 'gaulois', 'Panthéon gaulois');
+    }
+
+    #[Route('/pantheon/gallois', name: 'app_public_pantheon_gallois', methods: ['GET'])]
+    public function welsh(DieuRepository $repository): Response
+    {
+        return $this->renderPantheon($repository, 'gallois', 'Panthéon gallois');
+    }
+
+    #[Route('/pantheon/irlandais', name: 'app_public_pantheon_irlandais', methods: ['GET'])]
+    public function irish(DieuRepository $repository): Response
+    {
+        return $this->renderPantheon($repository, 'irlandais', 'Panthéon irlandais');
+    }
+
+    private function renderPantheon(DieuRepository $repository, ?string $pantheon, string $pageTitle): Response
+    {
+        $queryBuilder = $repository->createQueryBuilder('d')->orderBy('d.name', 'ASC');
+
+        if ($pantheon !== null) {
+            $queryBuilder
+                ->innerJoin('d.pantheons', 'p')
+                ->andWhere('LOWER(p.title) LIKE :pantheon')
+                ->setParameter('pantheon', '%'.$pantheon.'%')
+                ->distinct();
+        }
+
+        return $this->render('public/pantheon/index.html.twig', [
+            'dieux' => $queryBuilder->getQuery()->getResult(),
+            'activePantheon' => $pantheon ?? 'all',
+            'pageTitle' => $pageTitle,
+        ]);
+    }
+}
